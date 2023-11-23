b025916312e5940f7df4773d9e4e358470bf16e7
EduAir94 <49965850+EduAir94@users.noreply.github.com>
2023-11-23 01:14:32 -0300

    fix: AppDispatch

--- hooks/useFavourites.ts
+++ hooks/useFavourites.ts
@@ -1,11 +1,12 @@
 'use client'
+import { type AppDispatch } from '@/app/store'
 import { setFavourites } from '@/app/store/favourite/thunks'
 import confetti from 'canvas-confetti'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
 export const useFavourite = () => {
-  const dispatch = useDispatch()
+  const dispatch: AppDispatch = useDispatch()
   useEffect(() => {
     dispatch(setFavourites())
     window.confetti = confetti
