933434980ed0151a31dd194c235b05eafb08d49f
EduAir94 <49965850+EduAir94@users.noreply.github.com>
2023-11-23 01:24:14 -0300

    fix: Next13Progress bar suspense

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -1,8 +1,11 @@
 'use client'
 import { Next13ProgressBar } from 'next13-progressbar'
+import { Suspense } from 'react'
 
 export const Progress = () => {
   return (
-    <Next13ProgressBar height="4px" color="#0A2FFF" options={{ showSpinner: true }} showOnShallow />
+    <Suspense>
+      <Next13ProgressBar height="4px" color="#0A2FFF" options={{ showSpinner: true }} showOnShallow />
+    </Suspense>
   )
 }
